0fb992dc87f90135298308d8bc83055bcf339ca2
Eric Sauvageau <merlin@asuswrt-merlin.net>
2026-06-10 11:44:45 -0400

    rc: change method of restarting ntpd in watchdog
    (cherry picked from commit c701eb293a1d9f59b2f378706e3b8438bee9bea8)

--- release/src/router/rc/watchdog.c
+++ release/src/router/rc/watchdog.c
@@ -11877,8 +11877,7 @@ wdp:
 	if (++ntpd_timer >= DAY_PERIOD) {
 		ntpd_timer = 0;
 		logmessage("ntpd", "Daily service restart");
-		stop_ntpd();
-		start_ntpd();
+		notify_rc("restart_ntpd");
 	}
 #endif
 #if defined(GTBE98)
